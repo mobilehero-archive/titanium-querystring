3a2a6a08627891d3dfd929060ec1e04c9b076656
Brenton House <brenton.house@gmail.com>
2019-07-07 17:07:43 -0500

    initial commit

---
+++ querystring.bootstrap.js
@@ -0,0 +1,3 @@
+require('/ti.internal/extensions/binding')('querystring', '/node_modules/@titanium/querystring');
+require('/ti.internal/extensions/binding')('qs', '/node_modules/@titanium/querystring');
+
