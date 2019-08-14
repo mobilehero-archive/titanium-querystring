05ed40deb7d3bc8a7fbbfdc333d5a964c602cd35
Brenton House <brenton.house@gmail.com>
2019-08-14 15:05:47 -0500

    updates for 8.1.0.GA SDK

--- querystring.bootstrap.js
+++ querystring.bootstrap.js
@@ -1,3 +1,8 @@
-require('/ti.internal/extensions/binding')('querystring', '/node_modules/@titanium/querystring');
-require('/ti.internal/extensions/binding')('qs', '/node_modules/@titanium/querystring');
+// code for SDK < 8.1.0.GA
+// require('/ti.internal/extensions/binding')('querystring', '/node_modules/@titanium/querystring');
+// require('/ti.internal/extensions/binding')('qs', '/node_modules/@titanium/querystring');
+
+// Code for SDK >= 8.1.0.GA
+global.binding.register('querystring', require('@titanium/querystring'));
+global.binding.register('qs', require('@titanium/querystring'));
 
